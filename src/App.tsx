6a9ca3d37c799035206017627d4156cc188b0c1c
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-01-11 00:09:05 +0530

    images

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from './store/hooks'
+import { updateBoard } from './store';
+import { createBoard } from './utils/createBoard';
 
 function App ()  {
+  const dispatch = useAppDispatch();
+
+  const board = useAppSelector(({candyCrush:{board}}) => board);
+  const boardSize = useAppSelector(({candyCrush:{boardSize}}) => boardSize);
+
+  useEffect(()=>{
+    dispatch(updateBoard(createBoard(boardSize)))
+   
+  }, [boardSize, dispatch]);
+
   return (
     <div>App</div>
   )
