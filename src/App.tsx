c3df44303c5857e975231339871dcf763a4ffc0e
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-01-11 01:35:21 +0530

    formulas

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { useAppDispatch, useAppSelector } from './store/hooks'
 import { updateBoard } from './store';
 import { createBoard } from './utils/createBoard';
 import Board from './components/Board';
+import { checkForColumnOfThree, checkForRowOfFour, checkForRowOfThree, isColumnOfFour } from './utils/moveCheckLogic';
+import { formulaForColumnOfFour, formulaForColumnOfThree, generateInvalidMoves } from './utils/formulas';
 
 function App ()  {
   const dispatch = useAppDispatch();
@@ -14,6 +16,27 @@ function App ()  {
     dispatch(updateBoard(createBoard(boardSize)))
    
   }, [boardSize, dispatch]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const newBoard = [...board];
+      isColumnOfFour(newBoard, boardSize, formulaForColumnOfFour(boardSize));
+      checkForRowOfFour(
+        newBoard,
+        boardSize,
+        generateInvalidMoves(boardSize, true)
+      );
+      checkForColumnOfThree(
+        newBoard,
+        boardSize,
+        formulaForColumnOfThree(boardSize)
+      );
+      checkForRowOfThree(newBoard, boardSize, generateInvalidMoves(boardSize));
+      dispatch(updateBoard(newBoard));
+      //dispatch(());
+    }, 150);
+    return () => clearInterval(timeout);
+  }, [board, dispatch, boardSize]);
 
   return (
     <div className="flex items-center justify-center h-screen">
